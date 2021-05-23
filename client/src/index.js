47b9c16fe41686b8861e29ab63a12d92fd4415ed
Jason Root <hello@jasonroot.com>
2021-05-23 13:52:20 -0600

    initial commit

---
+++ client/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import TodoList from './TodoList';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
