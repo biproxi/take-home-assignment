e8fa59830500477d9e69d604119ce463aeff35bf
Alex Lee <ahjlee712@gmail.com>
2021-08-05 19:30:15 -0400

    setup intial post reducers

--- biproxi/src/index.tsx
+++ biproxi/src/index.tsx
@@ -1,7 +1,15 @@
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+// import reducers from './reducers';
+
+// const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <App />,
+  // <Provider store = {store}>
+    <App />,
+  // </Provider>,
   document.getElementById('root')
 );
