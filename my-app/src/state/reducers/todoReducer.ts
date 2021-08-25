3c578c1b2db4d1c27c39f9262a95eb9902108d9d
Kevin <kxkleiman@gmail.com>
2021-08-24 19:57:12 -0600

    forgot to add

---
+++ my-app/src/state/reducers/todoReducer.ts
@@ -0,0 +1,29 @@
+// Ripped directly from README
+enum TodoStatusEnum {
+    Active = 'Active', // the todo has is not completed
+    Inactive = 'Inactive', // the todo is completed
+    Archived = 'Archived', // the todo is archived
+};
+
+interface Todo {
+    title: string;
+    status: TodoStatusEnum;
+    lastUpdatedAt: number
+    createdAt: number;
+};
+
+type Actions = "ADD_TODO" | "DELETE_TODO" | "EDIT_TODO";
+type Action = {type: Actions, payload: Todo};
+
+export const todoReducer = (state: Todo[], action: Action) => {
+    switch(action.type) {
+        case "ADD_TODO":
+            return [...state, action.payload];
+        case "DELETE_TODO":
+            return [...state.filter(todo => {
+                    return todo === action.payload;
+                })]
+        case "EDIT_TODO":
+            return [...state]
+    }
+}
