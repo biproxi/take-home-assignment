a004fce43c453894d1d485dfc8f4ccc7900a5c44
Kevin <kxkleiman@gmail.com>
2021-08-25 10:08:07 -0600

    added declaration

---
+++ frontend/src/types.d.ts
@@ -0,0 +1,16 @@
+// Ripped directly from README
+export enum TodoStatusEnum {
+    Active = 'Active', // the todo has is not completed
+    Inactive = 'Inactive', // the todo is completed
+    Archived = 'Archived', // the todo is archived
+}
+
+export interface Todo {
+    title: string;
+    status: TodoStatusEnum;
+    lastUpdatedAt: number
+    createdAt: number;
+}
+
+export type Actions = "ADD_TODO" | "DELETE_TODO" | "EDIT_TODO";
+export type Action = {type: Actions, payload: Todo};
