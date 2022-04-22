ea4a274e4650166b157e984a3f3ba77a9c3cc26a
stryder03 <stryder03@users.noreply.github.com>
2022-04-22 17:28:57 -0600

    chore(cleanup): removed unused files

--- biproxi/src/pages/utils/redux/store.ts
+++ biproxi/src/pages/utils/redux/store.ts
@@ -2,13 +2,11 @@ import { configureStore } from '@reduxjs/toolkit'
 // Or from '@reduxjs/toolkit/query/react'
 import { setupListeners } from '@reduxjs/toolkit/query'
 import {todoApi} from './services/todoQueries'
-import needsUpdateSliceReducer from './features/needsUpdate'
 
 export const store = configureStore({
   reducer: {
     // Add the generated reducer as a specific top-level slice
     [todoApi.reducerPath]: todoApi.reducer,
-    needsUpdate: needsUpdateSliceReducer,
     },
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
