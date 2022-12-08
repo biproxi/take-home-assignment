154d1bd5e3e4b4033b4786445ba24492b4cde7e5
Kade Illian <kadeillianmt@gmail.com>
2022-12-07 23:48:22 -0700

    Tailwind is implemented!  Created the routes for the three pages and the associated stylesheets.  Also created dbConnect and the models dir

---
+++ inconceivable-movie-reviews/apps/inconceivable-movie-reviews/pages/display.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function  Display() {
+  return(
+    <div className="bg-indigo-500 p-2 font-mono">
+      <p>dis</p>
+    </div>
+  )
+}
+
+export default Display
---
+++ inconceivable-movie-reviews/apps/inconceivable-movie-reviews/pages/modify.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function  Modify() {
+  return(
+    <div className="text-3xl font-bold underline">
+      <p>I need Tailwind</p>
+    </div>
+  )
+}
+
+export default Modify
