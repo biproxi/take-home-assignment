92f5a18b0b27e5bd9689d27be36b93b8838a35ec
Kade Illian <kadeillianmt@gmail.com>
2022-12-11 17:14:59 -0700

    working delete method

--- inconceivable-movie-reviews/apps/inconceivable-movie-reviews/pages/display/[id].tsx
+++ inconceivable-movie-reviews/apps/inconceivable-movie-reviews/pages/display/[id].tsx
@@ -1,41 +1,64 @@
-// import Image from "next/image";
-// import React from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import dbConnect from "../../lib/dbConnect";
+import Movie from "../../models/Movie";
 
-// export function MovieShow(props) {
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     const params = new FormData(event.target);
-//     props.onUpdateMovie(props.movie.id, params);
-//     event.target.reset();
-//   };
+export function MovieShow({movie}) {
+  const router = useRouter()
+  const [message, setMessage] = useState('')
+  const handleDelete = async () => {
+    const movieId = router.query.id
 
-//   const handleClick = () => {
-//     props.onDestroyMovie(props.movie);
-//   };
+    try {
+      await fetch(`/api/movie/${movieId}`, {
+        method: 'Delete',
+      })
+      router.push('/')
+    } catch (error) {
+      setMessage('Failed to delete the movie.')
+    }
+  }
 
-//   return (
-//     <div>
-//       <h1>{props.movie.title}</h1>
-//       <Image src={props.movie.image} alt={""}></Image>
-//       <p>Movie Rating: {props.movie.movieRating}</p>
-//       <p>Tagline: {props.movie.tagline}</p>
-//       <p>Star Actor: {props.movie.starActor}</p>
+  return (
+    <div>
+      <h1>{movie.title}</h1>
+      <Image src={movie.image} alt={""}></Image>
+      <p>Movie Rating: {movie.movieRating}</p>
+      <p>Tagline: {movie.tagline}</p>
+      <p>Star Actor: {movie.starActor}</p>
 
-//       <button onClick={handleClick}>Destroy movie</button>
+      <button className="btn delete" onClick={handleDelete}>
+              Delete
+            </button>
 
-//       <h2>Update Mobie</h2>
-//       <form onSubmit={handleSubmit}>
-//         <div>
-//           Name: <input type="text" name="name" defaultValue={props.movie.name} />
-//         </div>
-//         <div>
-//           Price: <input type="number" name="price" defaultValue={props.movie.price} />
-//         </div>
-//         <div>
-//           Description: <input type="text" name="description" defaultValue={props.movie.description} />
-//         </div>
-//         <button type="submit">Update movie</button>
-//       </form>
-//     </div>
-//   )
-// }
+      <h2>Update Movie</h2>
+      <form>
+        <div>
+          Name: <input type="text" name="name" defaultValue={movie.name} />
+        </div>
+        <div>
+          Price: <input type="number" name="price" defaultValue={movie.price} />
+        </div>
+        <div>
+          Description: <input type="text" name="description" defaultValue={movie.description} />
+        </div>
+        <button type="submit">Update movie</button>
+      </form>
+      {message && <p>{message}</p>}
+    </div>
+  )
+}
+
+export async function getServerSideProps({ params }) {
+  await dbConnect()
+
+  const movie = await Movie.findById(params.id).lean()
+  movie._id = movie._id.toString()
+  movie.createdAt = ""
+  movie.updatedAt = ""
+
+  return { props: { movie } }
+}
+
+export default MovieShow
--- inconceivable-movie-reviews/apps/inconceivable-movie-reviews/pages/display/index.tsx
+++ inconceivable-movie-reviews/apps/inconceivable-movie-reviews/pages/display/index.tsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import Image from "next/image";
 import dbConnect from "../../lib/dbConnect";
 import Movie from "../../models/Movie";
+import Link from "next/link";
 
 export function Display(props) {
   const [searchFilter, setSearchFilter] = useState("");
+
   return(
     <div className="row">
       Search filter:{" "}
@@ -17,15 +19,13 @@ export function Display(props) {
       {props.movies
         .filter((movie) => movie.title.toLowerCase().includes(searchFilter))
         .map((movie) => (
-          <div key={movie.id}>
-            <h2>{movie.title}</h2>
-            <Image src={movie.imageURL} alt={""}></Image>
-            <p>{movie.tagline}</p>
-            <p>{movie.starActor}</p>
-            <p>{movie.movieRating}</p>
-            {/* <button onClick={() => props.onSelectProduct(product)} className="btn btn-info">
-              More info
-            </button> */}
+          <div key={movie.id} className="flex justify-center">
+            <h2 className="text-gray-900 text-xl font-medium mb-2">{movie.title}</h2>
+            <Image className="rounded-t-lg" src={movie.imageURL} width={0} height={0} alt={""}></Image>
+            <p className="text-gray-700 text-base mb-4">Tagline: {movie.tagline}</p>
+            <p className="text-gray-700 text-base mb-4">Star Actor: {movie.starActor}</p>
+            <p className="text-gray-700 text-base mb-4">Rating: {movie.movieRating}</p>
+            <Link type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" href={`/display/${movie.id}`} >More Info</Link>
           </div>
         ))}
     </div>
